d4ebb7fc5690c953539592f9b432a8b9e6402531
Gathukumi <gathukumidavid001@gmail.com>
2023-10-05 09:44:58 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+document.querySelector("#main").remove();
+
+const victoryHeader = document.querySelector("h1#victory");
+
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = 'victory';
+newHeader.textContent = 'David is the champion';
